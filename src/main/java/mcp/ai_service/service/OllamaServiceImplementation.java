e19a94734e1b2b32bad2aebac8857a0a55b9e97d
Srijan Singh <Srijan.Singh1@ibm.com>
2025-06-16 11:20:18 +0530

    2: custom prompt

--- src/main/java/mcp/ai_service/service/OllamaServiceImplementation.java
+++ src/main/java/mcp/ai_service/service/OllamaServiceImplementation.java
@@ -3,6 +3,7 @@ package mcp.ai_service.service;
 import lombok.extern.slf4j.Slf4j;
 import mcp.ai_service.tool.ToolConfig;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,12 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class OllamaServiceImplementation implements AIService {
+
+    /**
+     * Context or instruction to the assistant on how it should behave during the conversation.
+     */
+    @Value("${chat.context.user-info}")
+    private String chatContextUserInfo;
 
     private ChatClient chatClient;
 
@@ -21,6 +28,7 @@ public class OllamaServiceImplementation implements AIService {
     @Override
     public String askModel(String question) {
         var content = chatClient.prompt()
+                .system(chatContextUserInfo) // set the system prompt
                 .user(question)
                 .functions(ToolConfig.USER_TOOL)
                 .call()
