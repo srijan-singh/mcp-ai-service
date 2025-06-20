d901692c78938473053f647f8706bf9785985eb4
Srijan Singh <Srijan.Singh1@ibm.com>
2025-06-20 12:38:41 +0530

    update import

--- src/main/java/mcp/ai_service/controller/AIController.java
+++ src/main/java/mcp/ai_service/controller/AIController.java
@@ -2,6 +2,7 @@ package mcp.ai_service.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import mcp.ai_service.pojo.UserData;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
